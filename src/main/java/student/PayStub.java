a17d4dc64372a2cd6ac293046c1be6c2893268db
liangchiehho <you@example.com>
2025-02-05 13:48:14 -0800

    update payStub

--- src/main/java/student/PayStub.java
+++ src/main/java/student/PayStub.java
@@ -1,49 +1,56 @@
 package student;
 
+import java.math.BigDecimal;
+
 /**
  * pay stub including payment details for a pay period.
  */
 public class PayStub implements IPayStub {
     /** The employee with the pay stub. */
-    private IEmployee employee;
-    /** The gross pay amount for the pay period. */
-    private double pay;
+    private final IEmployee employee;
+    /** The net pay amount for the pay period. */
+    private final BigDecimal netPay;
     /** The taxes paid amount for the pay period. */
-    private double taxesPaid;
+    private final BigDecimal taxes;
 
     /**
      * create a new paystub.
      * @param employee the employee this paystub is for
-     * @param pay the gross pay amount
-     * @param taxesPaid the taxes paid amount
+     * @param netPay the net pay amount
+     * @param taxes the taxes paid amount
      */
-    public PayStub(IEmployee employee, double pay, double taxesPaid) {
+    public PayStub(IEmployee employee, BigDecimal netPay, BigDecimal taxes) {
         if (employee == null) {
             throw new IllegalArgumentException("employee must not be null");
         }
-        if (pay < 0) {
+        if (netPay == null || netPay.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("pay must be greater than zero");
         }
-        if (taxesPaid < 0) {
+        if (taxes == null || taxes.compareTo(BigDecimal.ZERO) < 0) {
             throw new IllegalArgumentException("taxesPaid must be greater than zero");
         }
         this.employee = employee;
-        this.pay = pay;
-        this.taxesPaid = taxesPaid;
+        this.netPay = netPay;
+        this.taxes = taxes;
     }
 
     @Override
     public double getPay() {
-        return pay;
+        return netPay.doubleValue();
     }
 
     @Override
     public double getTaxesPaid() {
-        return taxesPaid;
+        return taxes.doubleValue();
     }
 
     @Override
     public String toCSV() {
-        return String.format("%s,%.2f,%.2f", employee.getName(), pay, taxesPaid);
+        return String.format("%s,%.2f,%.2f,%.2f,%.2f",
+                employee.getName(),
+                getPay(),
+                getTaxesPaid(),
+                employee.getYTDEarnings(),
+                employee.getYTDTaxesPaid());
     }
 }
